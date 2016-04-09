6b72751114a07e3029849b0e473e723bb6629abb
Raymond <ray@ubuntu.master>
2016-04-10 00:48:51 +0800

    daily

--- app_client/controller/map.controller.js
+++ app_client/controller/map.controller.js
@@ -9,13 +9,59 @@
     function mapController($scope) {
         console.log("inside mapController ...");
 
-        angular.extend($scope, {
-            myLocation: {
-                lng: 114.156296,
-                lat: 22.319480,
-                zoom: 14,
-            }
-        });
+        var vm = this;
+        vm.myLocation = {
+            lng: 114.156296,
+            lat: 22.319480,
+            zoom: 14,
+        };
+        vm.events = {};
+        vm.markers = new Array();
 
+        $scope.$on("leafletDirectiveMap.click", function(event, args) {
+            console.log("in leafletDirectiveMap.click");
+            var leafEvent = args.leafletEvent;
+
+            //            if (typeof Obj.property == "undefined") {
+            //                // Assign value to the property here
+            //                Obj.property = someValue;
+            //            }
+            if (typeof(vm.startPos) == "undefined") {
+                vm.startPos = {
+                    lat: leafEvent.latlng.lat,
+                    lng: leafEvent.latlng.lng,
+                };
+                vm.markers.push({
+                    lat: leafEvent.latlng.lat,
+                    lng: leafEvent.latlng.lng,
+                    message: "My Route start at here!"
+                });
+            } else if (typeof(vm.endPos) == "undefined") {
+                vm.endPos = {
+                    lat: leafEvent.latlng.lat,
+                    lng: leafEvent.latlng.lng,
+                };
+                vm.markers.push({
+                    lat: leafEvent.latlng.lat,
+                    lng: leafEvent.latlng.lng,
+                    message: "My Route end at here!"
+                })
+            } else {
+                delete vm.startPos;
+                delete vm.endPos;
+                vm.markers = new Array();
+
+                vm.startPos = {
+                    lat: leafEvent.latlng.lat,
+                    lng: leafEvent.latlng.lng,
+                };
+                vm.markers.push({
+                    lat: leafEvent.latlng.lat,
+                    lng: leafEvent.latlng.lng,
+                    message: "My Route start at here!"
+                });
+            } // if vm.startPos and vm.endPos
+
+        }); // leafletDirectiveMap.click
     }; // mapController
 })(); // function()
